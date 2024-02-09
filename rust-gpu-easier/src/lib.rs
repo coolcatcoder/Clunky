0c6daa7bfd38f74b20bf0fbfc5cdc7e915936117
coolcatcoder <coolcatcal5@gmail.com>
2024-02-09 09:59:41 +0800

    fixing the broken commit
    I don't know why last commit didn't work properly. This should fix.

---
+++ rust-gpu-easier/src/lib.rs
@@ -0,0 +1,63 @@
+extern crate proc_macro;
+use std::{env, path::Path, process::Command};
+
+use proc_macro::TokenStream;
+
+const CHANNEL: &str = "nightly-2023-09-30";
+
+#[proc_macro]
+pub fn wow(item: TokenStream) -> TokenStream {
+    let rust_file = item.to_string();
+    match Path::new(&rust_file).try_exists() {
+        Ok(true) => (),
+        Ok(false) => panic!("File '{}' does not exist.", rust_file.clone()),
+        Err(err) => panic!("{}",err),
+    }
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    // As far as I can tell, it is not possible to both create a binary and work out the crate name at the same time.
+    let get_crate_name = Command::new("rustc")
+        .current_dir(out_dir.clone())
+        .env_remove("CARGO_MAKEFLAGS")
+        .arg(rust_file.clone())
+        .arg("--print=crate-name")
+        .output()
+        .unwrap();
+    let mut crate_name = String::from_utf8(get_crate_name.stdout).unwrap();
+    crate_name.pop().unwrap();
+
+    let compile = Command::new("rustup")
+        .current_dir(out_dir.clone())
+        .env_remove("CARGO_MAKEFLAGS")
+        .arg("run")
+        .arg(CHANNEL)
+        .arg("rustc")
+        .arg(rust_file)
+        .output()
+        .unwrap();
+
+    // compile must succeed or else it could run an old binary still in OUT_DIR
+    if !compile.status.success() {
+        let compile_stderr = String::from_utf8(compile.stderr).unwrap();
+        panic!("{}", compile_stderr);
+    }
+
+    let run = Command::new(format!("./{}", crate_name))
+        .current_dir(out_dir)
+        .output()
+        .expect(format!("./{}", crate_name).as_str());
+
+    // Temp, please remove.
+    format!(
+        "\"compile: {}, {}, {}\nrun: {}, {}, {}\"",
+        compile.status,
+        String::from_utf8(compile.stderr).unwrap(),
+        crate_name,
+        run.status,
+        String::from_utf8(run.stderr).unwrap(),
+        String::from_utf8(run.stdout).unwrap()
+    )
+    .parse()
+    .unwrap()
+}
